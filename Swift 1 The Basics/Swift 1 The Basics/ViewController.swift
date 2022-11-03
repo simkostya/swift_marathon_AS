cd1816449c46869b1d1fc3849b53ea40385c7d4e
Костя Симончик <simonchik2210@gmail.com>
2022-11-03 21:57:55 +0100

    Swift 1. The Basics

--- Swift 1 The Basics/Swift 1 The Basics/ViewController.swift
+++ Swift 1 The Basics/Swift 1 The Basics/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var titleCard: UILabel!
+    @IBOutlet weak var nameStudent: UILabel!
+    @IBOutlet weak var surnameStudent: UILabel!
+    @IBOutlet weak var ageStudent: UILabel!
+    @IBOutlet weak var genderStudent: UILabel!
+    @IBOutlet weak var statusStudent: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleCard.text = "Student's Card"
+        nameStudent.text = "Name: Kostya"
+        surnameStudent.text = "Surname: Simonchik"
+        ageStudent.text = "Age: 29 years"
+        genderStudent.text = "Gender: Male"
+        statusStudent.text = "Status: Married"
+        
+        cardView.layer.cornerRadius = 10
+        cardView.layer.shadowColor = UIColor.black.cgColor
+        cardView.layer.shadowOpacity = 1
+        cardView.layer.shadowOffset = .zero
+        cardView.layer.shadowRadius = 100
     }
-
-
 }
 
