0c917f628c639280412566e02f72d7f4cea82ad9
Костя Симончик <simonchik2210@gmail.com>
2022-11-03 15:52:44 +0100

    Swift 1. The Basics

---
+++ Swift 1 The Basics/Swift 1 The Basics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift 1 The Basics
+//
+//  Created by Костя Симончик on 3.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
